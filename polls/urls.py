96a931a6ecb7669e0d73652d746e1f619c103e7d
mshibli786 <mshibli786@gmail.com>
2017-01-25 11:36:29 +0530

    second commit

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index1, name='index'),
-    url(r'^sam$',views.sam),
-    url(r'^getUsers/\d+$',views.getUsers),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[1-9]+)/results/$',views.results,name='results'),
+    url(r'^(?P<question_id>[1-9]+)/vote/$',views.vote,name='vote'),
 ]
